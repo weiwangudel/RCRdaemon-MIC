1d52e570879debfea6e0764ca7c494f9221090ad
Wei Wang <weiwang.udel@gmail.com>
2013-09-25 18:10:14 -0400

    Blackboard has been constructed. It displays the accumulative power consumption. The energy is represented as a double occupying 8 bytes. The blackboard related operations seems to only work in RCR.bb.c. I am not successful in arbitraily anchor a function.

--- RCR.bb.c
+++ RCR.bb.c
@@ -6,6 +6,7 @@
 #include <unistd.h>   // for ftruncate
 #include <sys/ioctl.h>
 #include <string.h>
+#include "RCR.bb.h"
 
 #define RCRFILE_NAME  "RCRMICFile"
 #define MAX_RCRFILE_SIZE 1024
@@ -13,18 +14,6 @@
 void *bbMem;
 int allocationHighWater;
 
-struct PAPI_MIC_COUNTERS {
-        int total0;
-        int total1;
-        int inst;
-        int imax;
-        int pcie;
-        int c2x3;
-        int c2x4;
-        int vccp;
-        int vddg;
-        int vddq;
-};
 
 char * base() {
 	return (char*) bbMem;
@@ -33,25 +22,20 @@ char * base() {
 // allocate next block of shared memory -- track highwater mark
 
 int64_t allocateSharedMemory(int64_t size) {
-	int64_t ret = allocationHighWater;
-	if (size == 0) size = 8; // put blank word in if no space required to prevent field overlaps
-	if (size + allocationHighWater > MAX_RCRFILE_SIZE) {
-		fprintf(stderr, " Out of shared memory\n");
-		exit(1);
-	}
-	if (ret == 0) { // no allocation invent some memory
-		int fd = shm_open(RCRFILE_NAME, O_RDWR | O_CREAT, 0);  // TODO should be #def or execution time definable
-		if (fd == -1) {
-			perror("shm_open");
-			exit(1);
-		}
-
-		ftruncate(fd, MAX_RCRFILE_SIZE);
-		bbMem = mmap(NULL, MAX_RCRFILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-	}
+    int fd = shm_open(RCRFILE_NAME, O_RDWR | O_CREAT, 0);  // TODO should be #def or execution time definable
+    if (fd == -1) {
+    	perror("shm_open");
+    	exit(1);
+    }
+    
+    ftruncate(fd, MAX_RCRFILE_SIZE);
+    bbMem = mmap(NULL, MAX_RCRFILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    
+    printf("in the allocateSharedMemory:%#x\n", bbMem);
 	allocationHighWater += size;
-	return ret;
+    printf("allocationHighWater:%d\n", allocationHighWater);
+//    initBlackboard();
+    return allocationHighWater;
 }
 
 // get current allocation offset -- next allocation starting offset
@@ -60,28 +44,24 @@ int64_t getCurOffset() {
 	return allocationHighWater;
 }
 
-/******************************
- *  reads current blackboard file and sets it up for access 
- *****************************/
-int initBlackBoard() {
-	int ret = -1; 
-
-	int fd = shm_open(RCRFILE_NAME, O_RDONLY, 0);
-
-	if (fd == -1) {
-		perror("shm_open "RCRFILE_NAME);
-		return -1;
-	}
-
-	ftruncate(fd, MAX_RCRFILE_SIZE);
-	bbMem = mmap(NULL, MAX_RCRFILE_SIZE, PROT_READ, MAP_SHARED, fd, 0);
-
-	return 1;
-}
-
-int64_t buildSystem(int64_t numBBMeters, int64_t numNodes) {
-	int64_t i;
+int64_t buildBlackboard() {
 	int64_t offset = allocateSharedMemory(sizeof(struct PAPI_MIC_COUNTERS));
 	return offset;
 }
 
+
+void initBlackboard() {
+    struct PAPI_MIC_COUNTERS *node = (struct PAPI_MIC_COUNTERS *) (base());
+    node->total0 = 0.0;
+    printf("node->tot0:%f\n", node->total0);
+}
+
+void update_blackboard(char* e_name, long long value, long long interval) {
+ //*(*)e.() += value*(1e-6)*interval*(1e-6);
+  //printf("in reading: %#x:\n", base());
+  struct PAPI_MIC_COUNTERS* node = (struct PAPI_MIC_COUNTERS*)(base() );
+  //printf("in reading: %#x:\n", node);
+  ////printf("energy increment: %f %f\n", interval*(1.0e-6), value*(1.0e-6)*interval*(1.0e-6));
+  node->total0 += value*(1.0e-6)*interval*(1.0e-6); 
+  printf("accumulative: %f\n", node->total0);
+}
---
+++ RCR.bb.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int64_t buildBlackboard();
+void updateBlackboard();
+void initBlackboard() ;
+struct PAPI_MIC_COUNTERS {
+        double total0;
+        double total1;
+        double inst;
+        double imax;
+        double pcie;
+        double c2x3;
+        double c2x4;
+        double vccp;
+        double vddg;
+        double vddq;
+};
